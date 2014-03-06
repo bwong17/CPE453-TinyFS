77059054c5b1806a855b34f17cea989a7f7fe43d
Barbara Wong <barbara.wong92@gmail.com>
2014-03-05 21:41:58 -0800

    openDisk and readBlock finished

--- libDisk.c
+++ libDisk.c
@@ -1,9 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "libDisk.h"
+#include "libTinyFS.h"
+
+
 int openDisk(char *filename, int nBytes) {
-	return 0;
+    int file;
+    int i;
+    char buffer[nBytes];
+
+    file = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if(file){
+        if(nBytes > 0){
+            for(i = 0; i < nBytes; i++)
+                buffer[i] = 0x41;
+            write(file, buffer, nBytes);
+        }
+        if(!nBytes)
+            return -1;
+    }
+    else
+        return -1;
+    return (int)file;
 }
 
 int readBlock(int disk, int bNum, void *block) {
-	return 0;
+    int bytesRead;
+    bytesRead = -1;
+    
+    lseek(disk, 0, SEEK_SET); 
+    
+    if ((bytesRead = pread(disk, block, BLOCKSIZE, bNum * BLOCKSIZE)) == -1) { 
+        printf("Error:%s %d\n", strerror(errno), disk);
+        return -1;
+    } else
+        return 0;
 }
 
 int writeBlock(int disk, int bNum, void *block) {
--- tinyFSDemo.c
+++ tinyFSDemo.c
@@ -1,5 +1,18 @@
 #include "libDisk.h"
+#include "libTinyFS.h"
+
+#include <stdio.h>
 
 int main(void) {
-	return 0;
+    int file;
+    char buffer[BLOCKSIZE];
+    
+    file = openDisk("test",256);
+    
+    printf("FD in main: %d\n", file);
+    readBlock(file, 0, buffer);
+    
+    printf("%s", buffer);
+
+    return 0;
 } 
